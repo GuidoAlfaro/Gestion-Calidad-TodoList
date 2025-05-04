a1973e9945ca8bdb5452db8638e4448ea7b9e792
HusKTsu <juan941188@gmail.com>
2025-05-04 11:47:12 -0400

    Tag.java

---
+++ src/main/java/bo/edu/ucb/todolist/entity/Tag.java
@@ -0,0 +1,22 @@
+package bo.edu.ucb.todolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Data
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Task> tasks;
+}
