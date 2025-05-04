23a59465c3f3ef14cb0fc98be4e0853a78d43a34
HusKTsu <juan941188@gmail.com>
2025-05-04 11:31:03 -0400

    Revert "JUAN COMMIT O.o"
    This reverts commit f5fed75b75911f9a2ba3c0e52f42ee62647626e3.

--- src/main/java/bo/edu/ucb/todolist/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package bo.edu.ucb.todolist.repository;
-
-import bo.edu.ucb.todolist.model.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
--- src/test/java/bo/edu/ucb/todolist/TodolistApplicationTests.java
+++ src/test/java/bo/edu/ucb/todolist/TodolistApplicationTests.java
@@ -2,13 +2,12 @@ package bo.edu.ucb.todolist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class TodolistApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }
