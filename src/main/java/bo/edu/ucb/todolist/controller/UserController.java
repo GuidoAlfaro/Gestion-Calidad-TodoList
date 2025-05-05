2b9ec5e19484c4a2e07b593b515952f904979f1b
Andrea Quiroga <andrea.quiroga@ucb.edu.bo>
2025-05-04 22:30:40 -0400

    UserController

---
+++ src/main/java/bo/edu/ucb/todolist/controller/UserController.java
@@ -0,0 +1,32 @@
+package bo.edu.ucb.todolist.controller;
+
+import bo.edu.ucb.todolist.entity.User;
+import bo.edu.ucb.todolist.dao.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // POST /users - Create a new user
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    // GET /users - Retrieve all users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
