78a6d47b38f74df3cc55128f1f0c53596a0d166b
HusKTsu <juan941188@gmail.com>
2025-05-05 22:30:16 -0400

    vuelta a la normalidad 2

--- src/main/java/bo/edu/ucb/todolist/dao/TaskRepository.java
+++ src/main/java/bo/edu/ucb/todolist/dao/TaskRepository.java
@@ -3,11 +3,8 @@ package bo.edu.ucb.todolist.dao;
 import bo.edu.ucb.todolist.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByStatus(String status);
     List<Task> findByUserId(Long userId);
-    List<Task> findByUserIdAndStatus(Long userId, String status);
-    Optional<Task> findByIdAndUserId(Long id, Long userId);
 }
