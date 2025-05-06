0046167c1cbe4604170cacbcc0f421cf0a381d7b
Andrea Quiroga <andrea.quiroga@ucb.edu.bo>
2025-05-05 22:58:51 -0400

    TagController completo

--- src/main/java/bo/edu/ucb/todolist/controller/TagController.java
+++ src/main/java/bo/edu/ucb/todolist/controller/TagController.java
@@ -8,25 +8,59 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/tags")
+@RequestMapping("/api/tags")
 public class TagController {
 
     @Autowired
     private TagRepository tagRepository;
 
-    // POST /tags - Create a new tag
+    // POST /api/tags - Create a new tag
     @PostMapping
     public ResponseEntity<Tag> createTag(@RequestBody Tag tag) {
         Tag savedTag = tagRepository.save(tag);
         return new ResponseEntity<>(savedTag, HttpStatus.CREATED);
     }
 
-    // GET /tags - Retrieve all tags
+    // GET /api/tags - Retrieve all tags
     @GetMapping
     public ResponseEntity<List<Tag>> getAllTags() {
         List<Tag> tags = tagRepository.findAll();
         return new ResponseEntity<>(tags, HttpStatus.OK);
+    }
+
+    // GET /api/tags/{id} - Retrieve a tag by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Tag> getTagById(@PathVariable Long id) {
+        Optional<Tag> tag = tagRepository.findById(id);
+        return tag.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    // PUT /api/tags/{id} - Update a tag
+    @PutMapping("/{id}")
+    public ResponseEntity<Tag> updateTag(@PathVariable Long id, @RequestBody Tag tagDetails) {
+        Optional<Tag> optionalTag = tagRepository.findById(id);
+        if (optionalTag.isPresent()) {
+            Tag tag = optionalTag.get();
+            tag.setName(tagDetails.getName()); // Asumiendo que Tag tiene un campo name
+            Tag updatedTag = tagRepository.save(tag);
+            return new ResponseEntity<>(updatedTag, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // DELETE /api/tags/{id} - Delete a tag
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable Long id) {
+        if (tagRepository.existsById(id)) {
+            tagRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
