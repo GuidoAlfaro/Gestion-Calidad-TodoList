e46d7582527a87fd378c1b5809cf106f104905c8
HusKTsu <juan941188@gmail.com>
2025-05-05 22:27:03 -0400

    mejora 2

--- src/main/java/bo/edu/ucb/todolist/dao/UserRepository.java
+++ src/main/java/bo/edu/ucb/todolist/dao/UserRepository.java
@@ -2,6 +2,9 @@ package bo.edu.ucb.todolist.dao;
 
 import bo.edu.ucb.todolist.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
 }
