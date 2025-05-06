c4f76dc5db1c162ce521c95d7c8997d8f778a473
HusKTsu <juan941188@gmail.com>
2025-05-05 22:29:50 -0400

    vuelta a la normalidad

--- src/main/java/bo/edu/ucb/todolist/dao/TagRepository.java
+++ src/main/java/bo/edu/ucb/todolist/dao/TagRepository.java
@@ -2,11 +2,6 @@ package bo.edu.ucb.todolist.dao;
 
 import bo.edu.ucb.todolist.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    Optional<Tag> findByName(String name);
-    boolean existsByName(String name);
-    List<Tag> findByUserId(Long userId);
 }
