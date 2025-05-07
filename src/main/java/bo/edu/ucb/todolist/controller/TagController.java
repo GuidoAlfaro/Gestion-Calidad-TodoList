40c6bbff52ccff34bad81203ba4431b3acc05f75
Andrea Quiroga <andrea.quiroga@ucb.edu.bo>
2025-05-06 22:55:12 -0400

    Update TagController.java

--- src/main/java/bo/edu/ucb/todolist/controller/TagController.java
+++ src/main/java/bo/edu/ucb/todolist/controller/TagController.java
@@ -2,13 +2,13 @@ package bo.edu.ucb.todolist.controller;
 
 import bo.edu.ucb.todolist.entity.Tag;
 import bo.edu.ucb.todolist.repository.TagRepository;
+import bo.edu.ucb.todolist.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tags")
@@ -34,33 +34,27 @@ public class TagController {
     // GET /api/tags/{id} - Retrieve a tag by ID
     @GetMapping("/{id}")
     public ResponseEntity<Tag> getTagById(@PathVariable Long id) {
-        Optional<Tag> tag = tagRepository.findById(id);
-        return tag.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        Tag tag = tagRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Etiqueta", "id", id));
+        return new ResponseEntity<>(tag, HttpStatus.OK);
     }
 
     // PUT /api/tags/{id} - Update a tag
     @PutMapping("/{id}")
     public ResponseEntity<Tag> updateTag(@PathVariable Long id, @RequestBody Tag tagDetails) {
-        Optional<Tag> optionalTag = tagRepository.findById(id);
-        if (optionalTag.isPresent()) {
-            Tag tag = optionalTag.get();
-            tag.setName(tagDetails.getName()); // Asumiendo que Tag tiene un campo name
-            Tag updatedTag = tagRepository.save(tag);
-            return new ResponseEntity<>(updatedTag, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        Tag tag = tagRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Etiqueta", "id", id));
+        tag.setName(tagDetails.getName());
+        Tag updatedTag = tagRepository.save(tag);
+        return new ResponseEntity<>(updatedTag, HttpStatus.OK);
     }
 
     // DELETE /api/tags/{id} - Delete a tag
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTag(@PathVariable Long id) {
-        if (tagRepository.existsById(id)) {
-            tagRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        Tag tag = tagRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Etiqueta", "id", id));
+        tagRepository.delete(tag);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
